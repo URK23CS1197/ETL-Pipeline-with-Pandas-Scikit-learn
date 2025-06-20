627793ca813a3c0b23f976852f8bd83910599b87
NIRANSON <153480009+URK23CS1197@users.noreply.github.com>
2025-06-20 11:18:32 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,14 +1,14 @@
-# task1_etl_pipeline.py
+
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 import joblib
 
-# Load sample dataset
+
 df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv')
 
-# Preprocessing pipeline
+
 numeric_features = ['age', 'fare']
 numeric_transformer = Pipeline(steps=[('scaler', StandardScaler())])
 
